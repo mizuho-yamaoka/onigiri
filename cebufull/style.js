b0054c6525b57f0f5e6f2d121d5821e24490c493
user <>
2018-09-28 15:16:17 +0800

    9/28css変更

--- cebufull/style.js
+++ cebufull/style.js
@@ -1,21 +1,27 @@
 // JavaScript Document
 //slick js
-$( document ).on( 'ready', function () {
-	$( ".regular" ).slick( {
+$(document).on('ready', function () {
+	$(".regular").slick({
 		dots: false,
 		infinite: true,
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		prevArrow: '<img src="img/prev.png" class="slide-arrow prev-arrow">',
-		nextArrow: '<img src="img/next.png" class="slide-arrow next-arrow">'
-	} );
-} );
+		nextArrow: '<img src="img/next.png" class="slide-arrow next-arrow">',
+		responsive: [{
+			breakpoint: 751,
+			settings: {
+				slidesToShow: 1
+			}
+		}]
+	});
+});
 
-		//ハンバーガーボタン
-$(function(){
-     $('.menu-trigger').on('click', function() {
-         $(this).toggleClass('active');
-         $("#overlay").fadeToggle();
-         return false;
-     });
- });
+//ハンバーガーボタン
+$(function () {
+	$('.menu-trigger').on('click', function () {
+		$(this).toggleClass('active');
+		$("#overlay").fadeToggle();
+		return false;
+	});
+});
