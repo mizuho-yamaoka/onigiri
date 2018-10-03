cfe08a34dbd732bd90e06252ef359336eb540449
一ノ瀬由芽 <srv123123@me.com>
2018-10-03 15:07:46 +0800

    登録の際に起きるエラーを解決しました。確認お願いします

--- Team_project/register/signup.php
+++ Team_project/register/signup.php
@@ -1,20 +1,10 @@
 <?php
-
     date_default_timezone_set('Asia/Manila'); //フィリピン時間に設定
     session_start();
-    $errors = array();
 
-    if (isset($_GET['action']) && $_GET['action'] == 'rewrite') {
-      $_POST['input_name'] = $_SESSION['register']['name'];
-      $_POST['input_email'] = $_SESSION['register']['email'];
-      $_POST['input_password'] = $_SESSION['register']['password'];
-      $_POST['input_gender'] = $_SESSION['register']['gender'];
-      $_POST['input_age'] = $_SESSION['register']['age'];
-      $_POST['input_school'] = $_SESSION['register']['school'];
-      $_POST['input_other'] = $_SESSION['register']['other'];
-
-      $errors['rewrite'] = true;
-    }
+echo '<pre>';
+var_dump ($_POST);
+echo '</pre>';
 
       $name = '';
       $email = '';
@@ -23,16 +13,30 @@
       $age = '';
       $school = '';
       $other = '';
-      $errors = array();
+    // バリデーションに引っかかった項目を入れる
+    $errors = array();
+
+
+      // $errors = array();
+
+      //バリデーション
 
     if(!empty($_POST)) {
-      $name = $_POST['input_name'];
-      $email = $_POST['input_email'];
-      $password = $_POST['input_password'];
-      $gender = $_POST['input_gender'];
-      $age = $_POST['input_age'];
-      $school = $_POST['input_school'];
-      $other = $_POST['input_other'];
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+      $str_age = $_POST['age'];
+      $age = (int) $str_age;
+      $school = $_POST['school'];
+      $other = $_POST['other'];
+
+      if(isset($_POST['gender'])){
+      $str_gender = $_POST['gender'];
+      $gender = (int) $str_gender;
+    }
+// echo '<pre>';
+// var_dump ($gender);
+// echo '</pre>';
 
         //ユーザー名の空チェック
       if($name == '') {
@@ -54,20 +58,17 @@
         }
 
         //画像名を取得
-        $file_name = '';
-        if(!isset($_GET['action'])) {
-          $file_name = $_FILES['input_img_name']['name'];
-        }
-        if(!empty($file_name)) {
-            //
-          $file_type = substr($file_name, -3);
-            $file_type = strtolower($file_type); //大文字が含まれていた場合すべて小文字化
-            if($file_type !='jpg' && $file_type !='png' && $file_type !='gif') {
-              $errors['img_name'] = 'type';
-            }
-          } else {
+        $file_name = $_FILES['img_name']['name'];
+        if (empty($file_name)) {
             $errors['img_name'] = 'blank';
-          }
+        }else{
+              $file_type = substr($file_name,-3);
+              echo '<br>';
+        if ($file_type != 'jpg' && $file_type != 'png' && $ext != 'gif') {
+            $errors['img_name'] = 'type';
+        }
+        }
+
 
           //性別の空チェック
           if($gender == '') {
@@ -76,7 +77,7 @@
 
           //年齢の空チェック
           if($age == '') {
-            $age['age'] = 'blank';
+            $errors['age'] = 'blank';
           }
 
           //語学学校チェック
@@ -94,23 +95,16 @@
             $date_str = date('YmdHis');
             $submit_file_name = $date_str . $file_name;
 
-            move_uploaded_file($_FILES['input_img_name']['tmp_name'], '../user_profile_img/' . $submit_file_name);
+            move_uploaded_file(
+              $_FILES['img_name']['tmp_name'], 
+              '../user_profile_img/' . $submit_file_name);
 
-            $_SESSION['register']['name'] = $_POST['input_name'];
-            $_SESSION['register']['email'] = $_POST['input_email'];
-            $_SESSION['register']['password'] = $_POST['input_password'];
-
-
+            $_SESSION['register'] = $_POST;
             $_SESSION['register']['img_name'] = $submit_file_name;
-
-            $_SESSION['register']['gender'] = $_POST['input_gender'];
-            $_SESSION['register']['age'] = $_POST['input_age'];
-            $_SESSION['register']['school'] = $_POST['input_school'];
-            $_SESSION['register']['other'] = $_POST['input_other'];
 
 
             header('Location: check.php');
-            exit();
+            // exit();
           }
         }
 
@@ -123,13 +117,14 @@
         <head>
           <meta charset="utf-8">
           <title>Team Project</title>
+
         </head>
         <body>
           <h2>Create Account</h2>
-          <form method="POST" action="signup.php" enctype="multipart/form-data">
+          <form action="signup.php" method="POST" enctype="multipart/form-data">
             <div>
               <label for="name">ユーザー名(Username)</label>
-              <input type="text" name="input_name" placeholder="username" value="<?php echo htmlspecialchars($name); ?>">
+              <input type="text" name="name" placeholder="username" value="<?php echo htmlspecialchars($name); ?>">
               <?php if(isset($errors['name']) && $errors['name'] == 'blank') { ?>
                 <p>Please enter username</p>
               <?php } ?>
@@ -137,68 +132,68 @@
 
             <div>
               <label for="email">メールアドレス(email)</label>
-              <input type="email" name="input_email" placeholder="example@.com" value="<?php echo htmlspecialchars($email); ?>">
+              <input type="email" name="email" placeholder="example@.com" value="<?php echo htmlspecialchars($email); ?>">
               <?php if(isset($errors['email']) && $errors['email'] == 'blank') { ?>
                 <p>Please enter email</p>
               <?php } ?>
             </div>
 
             <div>
-              <label for="email">パスワード(password)</label>
-              <input type="password" name="input_password" placeholder="password" value="">
-              <?php if(isset($errors['password']) && $errors['password'] == 'blank') { ?>
-                <p>Please enter password</p>
-              <?php } ?>
+              <label for="password">パスワード(password)</label>
+              <input type="password" name="password" placeholder="password" >
+              <?php if(isset($errors['password']) && $errors['password'] == 'blank'): ?>
+                <span class="red">Please Enter Password</span>
+              <?php endif; ?>
 
-              <!-- <?php if(isset($errors['password']) && $errors['password'] == 'length') { ?>
-                <p>Password must be 4 to 16 characters</p>
-                <?php } ?> -->
 
-                <?php if(!empty($errors)) { ?>
-                  <p class="text-danger">パスワードを再度入力して下さい(Please enter password again)</p>
-                <?php } ?>
-              </div>
+              <?php if(isset($errors['password'])&& $errors['password'] == 'length'):?>
+              <span class="red">パスワードをは4〜16文字で入力して下さい。<s></s></span>
+              <?php endif; ?>
+
 
             <div>
               <label for="img_name">プロフィール画像(profile image)</label>
-              <input type="file" name="input_img_name" accept="image/*">
-              <?php if(isset($errors['img_name']) && $errors['img_name'] == 'type') { ?>
+              <input type="file" name="img_name" accept="image/*">
+              <?php if(isset($errors['img_name']) && $errors['img_name'] == 'blank'):?>
+                <p>画像を選択して下さい(Pleae choose a your  image picture)</p>
+              <?php endif; ?>
+              <?php if(isset($errors['img_name']) && $errors['img_name'] == 'type'):?>
                 <p>拡張子が「jpg」「png」「gif」の画像を選択してください(Please select an image with the extension "jpg" "png" "gif")</p>
-              <?php } ?>
+              <?php endif; ?>
             </div>
 
 
             <div>
-              <input type="radio" name="input_gender" value="<?php echo htmlspecialchars($gender); ?>"> 男
-              <input type="radio" name="input_gender" value="<?php echo htmlspecialchars($gender); ?>"> 女
-              <input type="radio" name="input_gender" value="<?php echo htmlspecialchars($gender); ?>"> その他
-              <?php if(isset($errors['gender']) && $errors['gender'] == 'blank') { ?>
+              <input type="radio" name="gender" value="1">男 
+              <input type="radio" name="gender" value="2">女
+              <input type="radio" name="gender" value="3">その他
+              <?php if(isset($errors['gender']) && $errors['gender'] == 'blank'): ?>
                 <p>Please select your gender</p>
-              <?php } ?>
+              <?php endif; ?>
           </div>
 
             <div>
               <label for="age">年齢(Age)</label>
-              <input type="text" name="input_age" placeholder="age" value="<?php echo htmlspecialchars($age); ?>">
-              <?php if(isset($errors['age']) && $errors['age'] == 'blank') { ?>
+              <input type="text" name="age" placeholder="age" value="<?php echo $age; ?>">
+              <?php if(isset($errors['age']) && $errors['age'] == 'blank') : ?>
                 <p>Please enter your age</p>
-              <?php } ?>
+              <?php endif; ?>
             </div>
 
             <div>
               <label for="school">語学学校(School)</label>
-              <input type="text" name="input_school" placeholder="QQ English" value="<?php echo htmlspecialchars($school);?>">
-              <?php if(isset($errors['school']) && $errors['school'] == 'blank') { ?>
+              <input type="text" name="school" placeholder="QQ English" value="<?php echo htmlspecialchars($school);?>">
+              <?php if(isset($errors['school']) && $errors['school'] == 'blank') : ?>
                 <p>Please enter your school</p>
-              <?php } ?>
+              <?php endif; ?>
             </div>
 
             <div>
               <label for="other">備考(other)</label>
-              <input type="text" name="input_other"  value="<?php echo htmlspecialchars($other);?>">
-              <?php if(isset($errors['other']) && $errors['other'] == 'blank') { ?>
+              <input type="text" name="other"  value="<?php echo htmlspecialchars($other);?>">
+              <?php if(isset($errors['other']) && $errors['other'] == 'blank') : ?>
                 <p>Please enter your other</p>
-              <?php } ?>
+              <?php endif; ?>
             </div>
 
             <div>
--- Team_project/signin.php
+++ Team_project/signin.php
@@ -1,5 +1,72 @@
 <?php
     // サインイン処理
+   session_start();
+    require_once('dbconnect.php');
+
+echo '<pre>';
+var_dump ($_POST);
+echo '</pre>';
+
+    // サインインボタンが押されたら
+    // $_POSTが空でなければ
+    $errors = [];
+    $email = '';
+
+    if (!empty($_POST)) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        // バリデーション（emailとpasswordの入力空チェック）
+        if ($email != '' && $password != '') {
+            // データベースとの照合
+            // 入力されたメールアドレスとパスワードの組み合わせがusersテーブルに存在するか
+                // SELECT文を使ってレコードを読み込む
+                    // 一致するデータがあるかどうか（存在）するかどうか
+            // ①SQL文の文字をセットする
+            $sql = 'SELECT * FROM `users` WHERE `email` = ?';
+            // ②SQL文に含みたいデータを配列で用意する(タプル処理)
+            $data = [$email]; 
+            // ③SQL文の文字をprepare()にセットする
+            $stmt = $dbh->prepare($sql);
+            // ④実行する
+            $stmt->execute($data);
+            
+            // 使えない→使えるデータに変換したい
+            // フェッチする　fetch()
+                // object型→Array型に変換する処理
+            $record = $stmt->fetch(PDO::FETCH_ASSOC);
+            // echo '<pre>';
+            // var_dump ($record);
+            // echo '</pre>';
+            // 登録されたメールアドレスかチェック
+            if ($record == false) {
+                $errors['signin'] = 'failed';
+            }
+            // パスワードが一致するかチェック
+            // $record['password']はDBから取ってきたパスワード
+            if (password_verify($password,$record['password'])) {
+                  // 認証成功
+                  // サインインするユーザーのIDをセッションに保存
+              $_SESSION['cebufull']['id'] = $record['id'];
+              header('Location: blog_list.php');
+            }else{
+                // 認証失敗
+              $errors['signin'] = 'failed';
+            }
+            echo '<pre>';
+            var_dump ($errors['signin']);
+            echo '</pre>';
+
+
+            // SELECT文でメールアドレスとパスワードが一致するレコードをusersテーブルから読み込む
+            // データが1件読み込めれば存在するデータということでOK
+            // データが0件ならメールアドレスとパスワードの組み合わせが間違っっているということでNG
+        }else{
+            // エラーを出す
+            $errors['signin'] = 'blank';
+        }
+    }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -18,11 +85,11 @@
         <form method="POST" action="" enctype="multipart/form-data">
           <div class="form-group">
             <label for="email">メールアドレス(Email)</label>
-            <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+            <input type="email" name="email" class="form-control" id="email" placeholder="example@gmail.com">
           </div>
           <div class="form-group">
             <label for="password">パスワード(Password)</label>
-            <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
+            <input type="password" name="password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
           </div>
           <input type="submit" class="btn btn-info" value="サインイン">
         </form>
