697b1dc15108851a80786b4c5a483791d09d00ae
一ノ瀬由芽 <srv123123@me.com>
2018-10-03 15:30:50 +0800

    まとめてコミットします

---
+++ Team_project/register/thanks.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>CEBUFULL</title>
+</head>
+<body style="margin-top: 60px">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-8 col-xs-offset-2 thumbnail">
+        <h2 class="text-center content_header">アカウント作成完了(Account creation complete)</h2>
+        <p>ユーザーアカウントの作成が完了しました。(User account creation is complete)</p>
+          <a href="../signin.php" class="btn btn-info">サインイン(Sign in)</a>
+      </div>
+    </div>
+    </div>
+  </div>
+</body>
+</html>
