89c4c975b18edb6402e6884fce8c21b6dd2547c1
user <>
2018-10-03 16:25:21 +0800

    ファイル階層の変更

--- Team_project/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dsn = 'mysql:dbname=CEBUFULL;host=localhost';
-    $name = 'root';
-    $password='';
-    $dbh = new PDO($dsn, $name, $password);
-    // SQL文にエラーがあった際、画面にエラーを出力する設定
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbh->query('SET NAMES utf8');
-?>
