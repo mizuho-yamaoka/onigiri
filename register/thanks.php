42ec328e38a9ecf7f065cb153236a62187deed3f
user <>
2018-10-13 16:49:55 +0800

    1013css追加

--- register/thanks.php
+++ register/thanks.php
@@ -1,19 +1,26 @@
+<?php
+require( '../path.php' );
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf-8">
   <title>CEBUFULL</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
-<body style="margin-top: 60px">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-8 col-xs-offset-2 thumbnail">
-        <h2 class="text-center content_header">アカウント作成完了(Account creation complete)</h2>
+<body>
+	<header>
+		<?php include ('../header/header.php'); ?>
+	</header>
+  <div class="nAcac">
+    <div class="thanksAn">
+        <h2>アカウント作成完了(Account creation complete)</h2>
         <p>ユーザーアカウントの作成が完了しました。(User account creation is complete)</p>
           <a href="../signin.php" class="btn btn-info">サインイン(Sign in)</a>
-      </div>
     </div>
     </div>
   </div>
+		<?php include ('../footer/footer.php'); ?>
+
 </body>
 </html>
