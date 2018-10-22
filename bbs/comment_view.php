da804fa412c932130bb319d9cfc0fc9c169ca554
user <>
2018-10-22 16:06:12 +0800

    css

--- bbs/comment_view.php
+++ bbs/comment_view.php
@@ -1,16 +1,16 @@
 <div class="collapse" id="collapseComment<?php echo $feed["id"] ?>">
-    <div class="col-xs-12" style="margin-top:10px">
+    <div class="con_1">
         <?php foreach ($feed['comments'] as $comment): ?>
-            <p style="margin-top: 30px; margin-bottom: 30px">
-            
+            <div class="comments">
             <img src="../user_profile_img/<?php echo $comment['img_name']?>" width="40" class="img-circle">
-            <span style="border-radius: 100px!important; -webkit-appearance:none;background-color:#eff1f3;padding:10px;margin-top:10px;">
-                <a href="bbs_people.php?id=<?php echo $feed['user_id']?>"><?php echo $comment['name']?></a>
-                <?php echo $comment['comments'] ?></span>
-            </p>
+				<dl>
+				<dt><a href="bbs_people.php?id=<?php echo $feed['user_id']?>"><?php echo $comment['name']?></a></dt>
+                <dd><?php echo $comment['comments'] ?></dd>
+				</dl>
+            </div>
         <?php endforeach; ?>
-        <form method="post" class="form-inline" action="comment.php" role="comment">
-            <div class="form-group">
+        <form method="post" class="bbsComment" action="comment.php" role="comment">
+            <div>
                 <img src="../user_profile_img/<?php echo $user['img_name']; ?>" width="40" class="img-circle">
             </div>
             <div class="form-group">
