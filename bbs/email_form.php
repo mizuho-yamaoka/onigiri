c14bf58a701b2c83fe7eea32e3cea95f05d4749d
一ノ瀬由芽 <srv123123@me.com>
2018-10-23 16:04:16 +0800

    JOIN 機能（メール送信機能）できました

--- bbs/email_form.php
+++ bbs/email_form.php
@@ -4,10 +4,9 @@
         <p>メールアドレス送信後投稿者からの連絡をお待ちください：）</p>
         <input type="emai" name="joiner_email" placeholder="your e-mail adress" >
         <input type="hidden" name="poster_email" value="<?php echo $feed['email'] ?>">
+        <input type="hidden" name="feed_id" value="<?php echo $feed_id?>">
         <input type="submit" name="sumbit" value="SEND">
-        <?php if (isset($_SESSION['email'])): ?>
-          <p>送信完了（SENT）</p>
-        <?php endif; ?>
+
         
     </form>
 </div>
