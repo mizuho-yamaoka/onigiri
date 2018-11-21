b2b56f6455dd98b1d73be34f3b13a21a38fb2ffd
一ノ瀬由芽 <srv123123@me.com>
2018-11-21 13:36:04 +0800

    echo hoge

--- register/signup.php
+++ register/signup.php
@@ -1,8 +1,8 @@
 <?php
-require( '../path.php' );
+// require( '../path.php' );
 
-date_default_timezone_set( 'Asia/Manila' ); 
-session_start();
+// date_default_timezone_set( 'Asia/Manila' ); 
+// session_start();
 echo 'hoge';
 
 
